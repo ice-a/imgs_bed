92dce7317fe4051dff997f0e73cde240e746bac2
爱喝水的木子 <50564847+ice-a@users.noreply.github.com>
2024-07-01 08:59:00 +0800

    简单图床

---
+++ imgs.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, redirect, url_for, render_template
+from flask import send_from_directory, jsonify
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+# 保存文件夹
+UPLOAD_FOLDER = 'uploads/'
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
+
+@app.route('/uploads', methods=['POST'])
+def upload_file():
+    if 'image' not in request.files:
+        return redirect(request.url)
+    file = request.files['image']
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        return jsonify({'success': True, 'url': url_for('uploaded_file', filename=filename)})
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
---
+++ readme.md
@@ -0,0 +1,8 @@
+# 使用
+python -v -> 3.10
+pip install -r requirements.txt
+python imgs.py
+创建一个uploads文件夹用来保存上传的图片
+
+然后通过访问页面进行上传图片
+访问图片可以通过 ip:5000/uploads/20240701083240.png
